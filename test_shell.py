67720d3d75b80ce26f919af02ebfda8326f3d93e
Mehdi Mulani <mehdi@fb.com>
2013-12-24 15:41:37 -0500

    Add test for running echo!

--- test_shell.py
+++ test_shell.py
@@ -6,7 +6,9 @@ import pytest
 def shell(tmpdir):
   import sys
 
-  return pexpect.spawn(' '.join([sys.executable, 'server.py']))
+  ret = pexpect.spawn(' '.join([sys.executable, 'server.py']))
+  ret.setecho(False)
+  return ret
 
 def expect_msg_type(shell, type):
   try:
@@ -18,8 +20,48 @@ def expect_msg_type(shell, type):
   msg = json.loads(shell.before.decode().rstrip('\r\n'))
   assert msg["type"] == type
 
+def expect_msg(shell, msg, timeout=2):
+  try:
+    shell.expect('\r\n', timeout)
+  except pexpect.TIMEOUT:
+    print(shell.before)
+    assert 0
+
+  output_msg = json.loads(shell.before.decode().rstrip('\r\n'))
+  assert msg == output_msg
+
 def send_msg(shell, msg):
   shell.sendline(json.dumps(msg))
 
 def test_start_shell(shell):
   expect_msg_type(shell, 'directory_info')
+
+def test_run_echo(shell):
+  expect_msg_type(shell, 'directory_info')
+  send_msg(shell,
+    {
+      "type": "start_task",
+      "message": {
+        "identifier": "1",
+        "arguments": ["echo", "test"],
+      },
+    })
+
+  expect_msg(shell,
+    {
+      "type": "task_output",
+      "message": {
+        "output": "test\r\n",
+        "identifier": "1",
+      },
+    })
+
+  expect_msg(shell,
+    {
+      "type": "task_done",
+      "message": {
+        "method": "exit",
+        "code": 0,
+        "identifier": "1",
+      },
+    })
