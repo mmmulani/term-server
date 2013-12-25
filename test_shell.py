ea6653bc37572b752726014893098ba53bead6ef
Mehdi Mulani <mehdi@fb.com>
2013-12-25 11:14:29 -0500

    Add tests for input!

--- test_shell.py
+++ test_shell.py
@@ -1,39 +1,53 @@
 import json
 import pexpect
 import pytest
+import sys
 
 @pytest.fixture(scope="function")
 def shell(tmpdir, request):
-  import sys
 
   ret = pexpect.spawn(' '.join([sys.executable, 'server.py']))
   ret.setecho(False)
 
   def teardown():
     ret.sendline(json.dumps({"type":"exit"}))
-    ret.wait()
   request.addfinalizer(teardown)
 
   return ret
 
-def expect_msg_type(shell, type):
-  try:
-    shell.expect('\r\n', timeout=2)
-  except pexpect.TIMEOUT:
-    print(shell.before)
-    assert 0
-
-  msg = json.loads(shell.before.decode().rstrip('\r\n'))
-  assert msg["type"] == type
-
-def expect_msg(shell, msg, timeout=2):
+def get_shell_msg(shell, timeout):
   try:
     shell.expect('\r\n', timeout)
   except pexpect.TIMEOUT:
     print(shell.before)
     assert 0
 
-  output_msg = json.loads(shell.before.decode().rstrip('\r\n'))
+  try:
+    msg = json.loads(shell.before.decode().rstrip('\r\n'))
+    return msg
+  except ValueError:
+    # This will happen if we receive non-JSON which usually means
+    # a program error.
+    sys.stdout.write(shell.before.decode())
+    shell.expect(pexpect.TIMEOUT, timeout=1)
+    text = shell.before
+    sys.stdout.write(text.decode())
+    assert 0
+
+  return None
+
+def expect_msg_type(shell, type, timeout=2):
+  msg = get_shell_msg(shell, timeout)
+  assert msg["type"] == type, \
+    "Expected: {0}, got: {1} with full message of {2}".format(type, msg["type"], msg)
+
+def expect_msg_output(shell, output, timeout=2):
+  msg = get_shell_msg(shell, timeout)
+  assert msg["message"]["output"] == output, \
+    "Expected: {0}, got: {1} with full message of {2}".format(output, msg["message"]["output"], msg)
+
+def expect_msg(shell, msg, timeout=2):
+  output_msg = get_shell_msg(shell, timeout)
   assert msg == output_msg
 
 def send_msg(shell, msg):
@@ -76,7 +90,6 @@ def test_exit_codes(shell):
   expect_msg_type(shell, 'directory_info')
 
   def test_error_code(code):
-    import sys
     test_error_code.counter += 1
     send_msg(shell,
       {
@@ -200,3 +213,52 @@ def test_change_dir(shell, tmpdir):
         "identifier": "1",
       },
     })
+
+def test_terminal_input(shell):
+  expect_msg_type(shell, 'directory_info')
+
+  def str_to_hex(str_):
+    return ''.join("{:02x}".format(ord(c)) for c in str_)
+
+  send_msg(shell,
+    {
+      "type": "start_task",
+      "message": {
+        "identifier": "1",
+        "arguments": ["cat", "-"],
+      },
+    })
+
+  send_msg(shell,
+    {
+      "type": "handle_input",
+      "message": {
+        "identifier": "1",
+        "input": str_to_hex("test"),
+      },
+    })
+  expect_msg_output(shell, "test")
+
+  send_msg(shell,
+    {
+      "type": "handle_input",
+      "message": {
+        "identifier": "1",
+        "input": str_to_hex("testing\n"),
+      },
+    })
+  expect_msg_output(shell, "testing\r\n")
+
+  send_msg(shell,
+    {
+      "type": "handle_input",
+      "message": {
+        "identifier": "1",
+        # 0x4 is EOT, i.e. CTRL+D.
+        "input": "04",
+      },
+    })
+  expect_msg_output(shell, "testtesting\r\n")
+  expect_msg_output(shell, "^D\x08\x08")
+
+  expect_msg_type(shell, "task_done")
